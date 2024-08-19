c19840e4e5dd24e83655f70ae6cedb4d1925a6f6
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-08-20 01:10:41 +0300

    SubHeader Component

---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SubHeader = () => {
+  return (
+    <div className='bg-dark text-light p-3 fs-5'>
+      <div className='container-xl text-center'>
+        إعلامات وتحديثات المنصة تجدونها على <a href="https://t.me/darelom_students">قناة التليجرام.</a>
+      </div>
+    </div>
+  )
+}
+
+export default SubHeader
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import SideBar from '../components/SideBar'
 import Footer from '../components/Footer'
 import ScrollToTop from '../components/ScrollToTop'
+import SubHeader from '../components/SubHeader'
 
 const MainLayout = () => {
   const { pathname } = useLocation();
@@ -11,6 +12,7 @@ const MainLayout = () => {
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Header />
+      <SubHeader />
       <div className='container-xl flex-grow-1'>
         <div className='row py-3'>
           <div className='col'>
