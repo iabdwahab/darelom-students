c9ddf9d6291d882d8b2a79c85c53d7e3a951dcc0
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-12-04 00:25:08 +0200

    Update SubHeader.jsx

--- src/components/header/SubHeader.jsx
+++ src/components/header/SubHeader.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const SubHeader = () => {
   return (
-    <div className='subheader bg-dark text-light fs-6' style={{ padding: '11px 0' }}>
-      <div className='container-xl text-center'>
-        إعلامات وتحديثات المنصة: <a href="https://t.me/darelom_students">قناة التليجرام.</a>
+    <>
+      <div className='subheader bg-dark text-light fs-6' style={{ padding: '11px 0' }}>
+        <div className='container-xl text-center'>
+          إعلامات وتحديثات المنصة: <a href="https://t.me/darelom_students">قناة التليجرام.</a>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
