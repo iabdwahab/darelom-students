edef8663612baec8ef53bf423076cbd790cd3edd
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-07-04 09:17:31 +0300

    main.jsx: Remove unused imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { doc, setDoc } from 'firebase/firestore'
-import { firebaseDB } from './utils/firebaseInit.js'
-import { supabase } from './supabase/initializing.js'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
   <App />
-  // {/* </React.StrictMode>, */ }
 )
 
 document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme') || 'light');
