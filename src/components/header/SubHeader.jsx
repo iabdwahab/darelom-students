a99db49d0af97952ba4116af61fdfdf182042741
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-12-31 09:29:16 +0200

    Comment out second subheader section in SubHeader.jsx

--- src/components/header/SubHeader.jsx
+++ src/components/header/SubHeader.jsx
@@ -1,6 +1,6 @@
 // import { Link } from 'react-router-dom';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const SubHeader = () => {
   return (
@@ -10,14 +10,14 @@ const SubHeader = () => {
           إعلامات وتحديثات المنصة: <a href="https://t.me/darelom_students">قناة التليجرام.</a>
         </div>
       </div>
-      <div className='subheader bg-dark border-top text-light fs-6' style={{ padding: '11px 0' }}>
+      {/* <div className='subheader bg-dark border-top text-light fs-6' style={{ padding: '11px 0' }}>
         <div className='container-xl text-center'>
           يمكنكم الآن الاطلاع على: {''}
           <br />
           <span>ترتيب نتائج الفصل الدراسي الثاني 2025.</span>
 
         </div>
-      </div>
+      </div> */}
       {/* <div className='subheader bg-dark border-top text-light fs-6' style={{ padding: '11px 0' }}>
         <div className='container-xl text-center'>
           يمكنكم الآن الاطلاع على ترتيب الفرق 2025.
