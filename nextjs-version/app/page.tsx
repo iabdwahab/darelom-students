db1a0a4e3ec5010f1089053927014a4deeacd8ff
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-26 09:34:09 +0200

    Remove nextjs-version project directory.

--- nextjs-version/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="container">
-      <h1 className="text-4xl font-bold text-center py-4">منصة طلاب دار العلوم</h1>
-    </main>
-  );
-}
